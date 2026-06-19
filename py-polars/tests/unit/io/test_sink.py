d1271625e6d508958e00f4b221913c18784923fb
Simon Lin <simonlin.rqmmw@slmail.me>
2026-06-20 04:29:00 +1000

    c

--- py-polars/tests/unit/io/test_sink.py
+++ py-polars/tests/unit/io/test_sink.py
@@ -521,7 +521,9 @@ def test_sink_morsel_splitting_without_user_configuration(
 ) -> None:
     buf = io.BytesIO()
 
-    df = pl.concat([pl.select(pl.repeat(1, n)) for n in input_chunk_lengths])
+    df = pl.concat(
+        [pl.select(pl.repeat(1, n, dtype=pl.UInt8)) for n in input_chunk_lengths]
+    )
     assert df.to_series(0).chunk_lengths() == input_chunk_lengths
 
     df.write_ipc(buf)
